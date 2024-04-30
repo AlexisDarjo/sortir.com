1515c03727d30a1dae2aa83d605e930835334e15
BastienGOGUET <bastien.goguet2023@campus-eni.fr>
2024-04-30 14:34:33 +0200

    table acceuil

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,7 @@ import 'bulma/css/bulma.css';
  */
 import './styles/app.css';
 import './styles/header.css';
+import './styles/accueil.css'
 
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');

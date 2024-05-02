84433a2b5dc5900ab4c0b9fafcaf23e94a999ff0
BastienGOGUET <bastien.goguet2023@campus-eni.fr>
2024-05-02 14:19:48 +0200

    route yaml

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/sortie')]
+
 class SortieController extends AbstractController
 {
     #[Route('/', name: 'app_sortie_index', methods: ['GET'])]

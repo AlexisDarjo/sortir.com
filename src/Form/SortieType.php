150c18243d1a2e0c561d34e5536f90dd18afd9d3
Amélia <amelia.pelletier2023@campus-eni.fr>
2024-05-03 10:22:53 +0100

    form créer une sortie

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -2,15 +2,14 @@
 
 namespace App\Form;
 
-use App\Entity\Etat;
-use App\Entity\Lieu;
 use App\Entity\Site;
 use App\Entity\Sortie;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,31 +20,86 @@ class SortieType extends AbstractType
     {
         $builder
             ->add('nom', TextType::class,[
-                'label'=>'Nom de la sortie',
+                'label'=>'Nom de la sortie :',
                 'required'=>true,
+                'attr' => [
+                    'class' => 'input custom-input',
+                ],
+                'label_attr' => [
+                    'class' => 'label has-text-white', // Ajoutez la classe CSS souhaitée pour le libellé ici
+                ],
             ])
             ->add('dateHeureDebut', DateTimeType::class, [
-                'label'=> 'Date et heure de la sortie' ,
+                'label'=> 'Date et heure de la sortie :' ,
                 'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'input custom-input',
+                ],
+                'label_attr' => [
+                    'class' => 'label has-text-white', // Ajoutez la classe CSS souhaitée pour le libellé ici
+                ],
             ])
-            ->add('duree')
-            ->add('dateLimiteInscription', null, [
+            ->add('dateLimiteInscription', DateTimeType::class, [
+                'label'=> 'Date limite d\'inscription :' ,
                 'widget' => 'single_text',
+                'attr' => [
+                    'class' => 'input custom-input',
+                ],
+                'label_attr' => [
+                    'class' => 'label has-text-white', // Ajoutez la classe CSS souhaitée pour le libellé ici
+                ],
             ])
-            ->add('nbInscriptionsMax')
-            ->add('infosSortie')
-            ->add('etat', EntityType::class, [
-                'class' => Etat::class,
-                'choice_label' => 'id',
+            ->add('nbInscriptionsMax',IntegerType::class,[
+            'label'=> 'Nombre de places :' ,
+                'attr' => [
+                    'class' => 'input custom-input',
+                ],
+                'label_attr' => [
+                    'class' => 'label has-text-white', // Ajoutez la classe CSS souhaitée pour le libellé ici
+                ],
+
             ])
-            ->add('lieu', EntityType::class, [
-                'class' => Lieu::class,
-                'choice_label' => 'id',
+            ->add('duree',IntegerType::class,[
+                'label'=> 'Durée :' ,
+                'attr' => [
+                    'class' => 'input custom-input',
+                ],
+                'label_attr' => [
+                    'class' => 'label has-text-white', // Ajoutez la classe CSS souhaitée pour le libellé ici
+                ],
             ])
+
+
+            ->add('infosSortie',TextareaType::class,[
+                'label'=>'Description et infos :',
+                'required'=>false,
+                'attr' => [
+                    'rows' => 20,
+                    'class' => 'input custom-input',
+
+                ],
+                'label_attr' => [
+                    'class' => 'label has-text-white', // Ajoutez la classe CSS souhaitée pour le libellé ici
+                ],
+            ])
+
             ->add('siteOrganisateur', EntityType::class, [
                 'class' => Site::class,
-                'choice_label' => 'id',
+                'choice_label' => 'nom',
+                'attr' => [
+                    'class' => 'input custom-input',
+                ],
+                'label_attr' => [
+                    'class' => 'label has-text-white', // Ajoutez la classe CSS souhaitée pour le libellé ici
+                ],
             ])
+
+            ->add('lieu', LieuType::class, [
+                'label' => false,
+                'required' => true,
+
+            ])
+
         ;
     }
 
